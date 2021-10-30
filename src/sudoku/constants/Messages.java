3ca74dc2c4d8e88daeacbd955f96ab7007e5a6a7
myusuf <myusufozcelik96@gmail.com>
2021-10-30 12:02:31 +0300

    sudoku app created

---
+++ src/sudoku/constants/Messages.java
@@ -0,0 +1,6 @@
+package sudoku.constants;
+
+public class Messages {
+    public static final String GAME_COMPLETE = "Tebrikler, kazandınız! Tekrar oynamak ister misiniz?";
+    public static final String ERROR = "Bir hata meydana geldi.";
+}
